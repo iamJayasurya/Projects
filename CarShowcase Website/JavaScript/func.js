9f5b26fb1ca8931a87c563bcde8806db765d0a45
iamJayasurya <150885146+iamJayasurya@users.noreply.github.com>
2024-01-01 19:12:42 +0530

    Update func.js

--- CarShowcase Website/JavaScript/func.js
+++ CarShowcase Website/JavaScript/func.js
@@ -1,1 +1,20 @@
+const images = [
+    'CoolPic1.jpg',
+    'CoolPic2.jpg',
+    'CoolPic3.jpg',
+  ];
 
+  let currentIndex = 0;
+ let body=document.getElementById('body')
+  function changeBackgroundImage() {
+    body.style.backgroundImage = `url(./images/${images[currentIndex]})`;
+    body.style.backgroundsize=`cover`
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+  setInterval(changeBackgroundImage, 5000);
+
+
+
+
+
+        
